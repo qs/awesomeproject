fd2abe720b911669ed0c2044e3ebdd5e6c7ff09b
Johannes <j.laute3@googlemail.com>
2018-11-24 15:15:07 +0200

    api update

--- server.py
+++ server.py
@@ -4,12 +4,35 @@ from models import Wish
 from flask_api import FlaskAPI, status
 from collections import defaultdict
 from flask_pymongo import PyMongo
+import json
 database = {}
 
 
 app = FlaskAPI(__name__)
 app.config["MONGO_URI"] = "mongodb://awesome1:awesome1@ds063789.mlab.com:63789/awesomeproject"
 mongo = PyMongo(app)
+
+mongo.db.users.delete_many({})
+
+mongo.db.users.insert({
+    "username": "ivan",
+    "preferences": {
+        "cheap": 0.5,
+        "sustainability": 0.2,
+        "comfort": 3
+    },
+    "wishlist": [12312312, 124123123, 1231234123]
+})
+
+mongo.db.users.insert({
+    "username": "freya",
+    "preferences": {
+        "cheap": 0.5,
+        "sustainability": 0.2,
+        "comfort": 3
+    },
+    "wishlist": [12312312, 124123123, 1231234123]
+})
 
 
 @app.route("/add_user", methods=["POST"])
@@ -22,21 +45,32 @@ def add_user():
 @app.route("/update-wish-list", methods=["POST"])
 def update_wish_list():
     username = request.data.get("username")
-    wishlist = request.data.get("wishlist")
-    database[username] = wishlist
-    print(database[username])
-    return "", status.HTTP_200_OK
+    wishlist = json.loads(request.data.get("wishlist"))
+    data = mongo.db.users.find_one({"username": username})
+    print(data["_id"])
+    a = mongo.db.users.update({"username": username}, {
+        "$set": {"wishlist": wishlist}})
+    # database[username] = wishlist
+    print(a)
+    print(wishlist)
+    data = mongo.db.users.find_one({"username": username})
+    del data["_id"]
+    return data, status.HTTP_200_OK
 
 
-@app.route("/get_wish_list")
+@app.route("/get-wish-list")
 def get_wish_list():
     username = request.data.get("username")
-    return database[username], status.HTTP_200_OK
+    data = mongo.db.users.find_one({"username": username})
+    del data["_id"]
+    return data, status.HTTP_200_OK
 
 
 @app.route("/get_suggestion")
 def get_suggestion():
     user = request.data.get("username")
+
+    data = mongo.db.users.find({"username": user})
     # get preferences from db
     preference = "cheaper"
     #
